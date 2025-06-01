5a066aabb7c254d8b2cbab0c35a6c6a0c078335e
hankcht <hanktang930809@gmail.com>
2025-06-01 15:49:00 -0700

    e

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -57,7 +57,7 @@ stations_to_run_on_pt2 = [14,17,19,30,15,18]
 '''always remember to first input a new model in A0'''
 
 # Can Train if needed
-cmd = f'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/simpleCutForDL.py 13 4.4.25'
+cmd = f'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/simpleCutForDL.py 15 4.4.25'
     # 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/A1.5_StudyCNN.py data_data'
 RunMultipleJobs(cmd , jobName=f'genericBatchJob')
 

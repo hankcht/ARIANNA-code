b5fc50773a7b0299d01db1af176788a0e80fc2a6
hankcht <hanktang930809@gmail.com>
2025-06-16 16:27:21 -0700

    add memory

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -5,29 +5,33 @@ def RunMultipleJobs(commandtorun, jobName='Batchjob', runDirectory='/pub/tangch3
     cmd = f'{commandtorun}'
     print(f'running {cmd}')
 
-    #now adding settings for slurm scheduler
     header = "#!/bin/bash\n"
-    header += f"#SBATCH --job-name={jobName}            ##Name of the job.\n"
-    header += "#SBATCH -A sbarwick_lab                  ##Account to charge to\n"
-    header += "#SBATCH -p standard                          ##Partition/queue name\n"
-    header += "#SBATCH --time=3-00:00:00                ##Max runtime D-HH:MM:SS, 3 days free maximum\n"
-    header += "#SBATCH --nodes=1                        ##Nodes to be used\n"
-    header += "#SBATCH --ntasks=1                       ##Numer of processes to be launched\n"
-    header += "#SBATCH --cpus-per-task=1                ##Cpu's to be used\n"
-    header += "#SBATCH --mem=18G\n"		       
-
+    header += f"#SBATCH --job-name={jobName}         ##Name of the job.\n"
+    header += "#SBATCH -A sbarwick_lab                 ##Account to charge to\n"
+    header += "#SBATCH --partition=standard            ##Partition/queue name\n" 
+    header += "#SBATCH --time=3-00:00:00               ##Max runtime D-HH:MM:SS, 3 days free maximum\n"
+    header += "#SBATCH --nodes=1                       ##Nodes to be used\n"
+    
+    header += "#SBATCH --ntasks=30                     ##Number of processes to be launched (CPUs)\n" 
+    header += "#SBATCH --mem-per-cpu=6G                ##Requesting 6GB memory per CPU\n"
+    # If keep --mem, it will override --mem-per-cpu * --ntasks if it's a lower value.
+    # It's better to explicitly calculate and rely on mem-per-cpu * ntasks for clarity.
+    # header += "#SBATCH --mem=18G\n" 
+    
     header += "#SBATCH --output={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.out'))
     header += "#SBATCH --error={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.err'))
     header += "#SBATCH --mail-type=fail,end\n"
     header += "#SBATCH --mail-user=tangch3@uci.edu\n"
 
-    #Add software to the python path
+
     header += "export PYTHONPATH=$NuM:$PYTHONPATH\n"
     header += "export PYTHONPATH=$Nu:$PYTHONPATH\n"
     header += "export PYTHONPATH=$Radio:$PYTHONPATH\n"
- 
+   
     header += "module load python/3.8.0\n"
     header += "cd $pub/FROG\n"
+
+
 
     slurm_name = os.path.join(runDirectory, 'sh', os.path.basename(jobName)) + ".sh"
     print(f'running cmd {cmd}')

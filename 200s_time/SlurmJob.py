ae0b255d88656cf0fc2f86e1845ec7ea7bdaba27
hankcht <hanktang930809@gmail.com>
2025-06-16 16:39:34 -0700

    m

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -61,7 +61,7 @@ stations_to_run_on_pt2 = [14,17,19,30,15,18]
 '''always remember to first input a new model in A0'''
 
 # Can Train if needed
-cmd = f'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/simpleCutForDL.py 14 4.4.25 --partition 1'
+cmd = f'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/simpleCutForDL.py 14 4.4.25 --partition 2'
     #'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/A1.5_StudyCNN.py data_data'
     #  
     # 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/A0_Utilities.py'

457effad13f1b240a4eb2cbaa5811960c21a9dc2
hankcht <hanktang930809@gmail.com>
2025-06-18 16:22:06 -0700

    m

--- 200s_time/B1_BLcurve.py
+++ 200s_time/B1_BLcurve.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
     def find_curve_13(curve_x):
         curve_y = []
         x1, y1 = 5.5, 0.6
-        x2, y2 = 15, 0.8
+        x2, y2 = 20, 0.9
         x3, y3 = 20, 0.9
         x4, y4 = 40, 0.75
 
@@ -261,7 +261,7 @@ if __name__ == "__main__":
         curve_y = []
         x1, y1 = 5, 0.55
         x2, y2 = 10, 0.7
-        x3, y3 = 30, 0.8
+        x3, y3 = 30, 0.87
 
         log_x1 = np.log10(x1)
         log_x2 = np.log10(x2)
@@ -289,9 +289,11 @@ if __name__ == "__main__":
         curve_y = []
         x1, y1 = 4.3, 0.6
         x2, y2 = 10, 0.75
+        x3, y3 = 30, 0.85
 
         log_x1 = np.log10(x1)
         log_x2 = np.log10(x2)
+        log_x3 = np.log10(x3)
 
         for x in curve_x:
             if x <= x1:
@@ -301,8 +303,13 @@ if __name__ == "__main__":
                 C = y1 - m * log_x1
                 y = m * np.log10(x) + C
                 curve_y.append(y)
+            elif x2 < x <= x3:
+                m = (y3 - y2) / (log_x3 - log_x2)
+                C = y2 - m * log_x2
+                y = m * np.log10(x) + C
+                curve_y.append(y)
             else:
-                curve_y.append(y2) # Flatline after x2
+                curve_y.append(y3) # Flatline after x2
 
         return curve_y
 
@@ -358,10 +365,12 @@ if __name__ == "__main__":
     def find_curve_30(curve_x):
         curve_y = []
         x1, y1 = 4.5, 0.53
-        x2, y2 = 20, 0.7
+        x2, y2 = 10, 0.7
+        x3, y3 = 20, 0.8
 
         log_x1 = np.log10(x1)
         log_x2 = np.log10(x2)
+        log_x3 = np.log10(x3)
 
         for x in curve_x:
             if x <= x1:
@@ -371,8 +380,13 @@ if __name__ == "__main__":
                 C = y1 - m * log_x1
                 y = m * np.log10(x) + C
                 curve_y.append(y)
+            elif x2 < x <= x3:
+                m = (y3 - y2) / (log_x3 - log_x2)
+                C = y2 - m * log_x2
+                y = m * np.log10(x) + C
+                curve_y.append(y)
             else:
-                curve_y.append(y2) # Flatline after x2
+                curve_y.append(y3) # Flatline after x2
 
         return curve_y
 

86e9a0b6a8e4a3373399528bfb9388dbdc5dc327
hankcht <hanktang930809@gmail.com>
2025-06-27 11:57:46 -0700

    m

--- 200s_time/A0_Utilities.py
+++ 200s_time/A0_Utilities.py
@@ -255,10 +255,10 @@ def siminfo_forplotting(type, amp, simulation_date, templates_2016, templates_RC
         path += f'{amp}/{simulation_date}'
         if amp == '200s':
             sim = np.load(f'{path}/SimRCR_200s_NoiseTrue_forcedFalse_4363events_FilterTrue_part0.npy')
-            weights = np.load(f'SimWeights_SimRCR_200s_NoiseTrue_forcedFalse_4363events_part0.npy')
+            weights = np.load(f'{path}/SimWeights_SimRCR_200s_NoiseTrue_forcedFalse_4363events_part0.npy')
         elif amp == '100s':
             sim = np.load(f'{path}/SimRCR_100s_NoiseTrue_forcedFalse_4668events_FilterTrue_part0.npy')
-            weigths = np.load(f'SimWeights_SimRCR_100s_NoiseTrue_forcedFalse_4668events_part0.npy')
+            weigths = np.load(f'{path}/SimWeights_SimRCR_100s_NoiseTrue_forcedFalse_4668events_part0.npy')
 
     elif type == 'Backlobe':
         # not yet developed 

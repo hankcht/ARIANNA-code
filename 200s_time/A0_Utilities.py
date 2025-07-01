72c958bfa9eb31b800d6e68204bd6aa7b05c011e
hankcht <hanktang930809@gmail.com>
2025-07-01 10:54:21 -0700

    run old mod on new sim as well

--- 200s_time/A0_Utilities.py
+++ 200s_time/A0_Utilities.py
@@ -472,23 +472,30 @@ if __name__ == "__main__":
     station_id = [14,17,19,30]
     amp_type = '200s'
     for id in station_id:
-        path = f'/dfs8/sbarwick_lab/ariannaproject/rricesmi/numpy_arrays/'                                                                                                     #Set which amplifier to run on
-        RCR_path = f'simulatedRCRs/{amp_type}_2.9.24/'
-        backlobe_path = f'simulatedBacklobes/{amp_type}_2.9.24/'
-        rcr, sim_Backlobe = load_sim(path, RCR_path, backlobe_path, amp_type)
+        '''load old rcr'''
+        # path = f'/dfs8/sbarwick_lab/ariannaproject/rricesmi/numpy_arrays/'                                                                                                     #Set which amplifier to run on
+        # RCR_path = f'simulatedRCRs/{amp_type}_2.9.24/'
+        # backlobe_path = f'simulatedBacklobes/{amp_type}_2.9.24/'
+        # rcr, sim_Backlobe = load_sim(path, RCR_path, backlobe_path, amp_type)
+        '''load new rcr'''
+        sim_folder = f'/dfs8/sbarwick_lab/ariannaproject/rricesmi/simulatedRCRs/{amp_type}/5.28.25/'
+        sim_rcr = load_sim_rcr(sim_folder, noise_enabled=True, filter_enabled=True, amp=amp_type)
 
-
+        '''load old above curve'''
         # All_data_SNR, All_data_Chi, All_Traces, All_data_UNIX = load_data('AboveCurve_data', amp_type, id)
+        '''load new above curve'''
         Above_curve_data_SNR, Above_curve_data_Chi2016, Above_curve_data_ChiRCR, All_Traces, Above_curve_data_UNIX = load_data('new_chi_above_curve', amp_type, id)
         
+        '''select 200 events for each station'''
         All_Traces = np.array(All_Traces)
-
         num_total_events = All_Traces.shape[0]
         selected_indices = np.random.choice(num_total_events, 200, replace=False)
         random_1000_events = All_Traces[selected_indices]
 
+        '''predict with old model'''
         network_output = RunTrainedModel(random_1000_events, '/pub/tangch3/ARIANNA/DeepLearning/models/')
-        prob_RCR = RunTrainedModel(rcr, '/pub/tangch3/ARIANNA/DeepLearning/models/')
+        prob_RCR = RunTrainedModel(sim_rcr, '/pub/tangch3/ARIANNA/DeepLearning/models/')
+        '''predict with new model'''
         # model = keras.models.load_model(f'/pub/tangch3/ARIANNA/DeepLearning/models/200s_time/new_chi_2025-06-20_16-06_RCR_Backlobe_model_2Layer.h5')
         # network_output = model.predict(random_1000_events)
         # prob_RCR = model.predict(rcr)
@@ -524,6 +531,9 @@ if __name__ == "__main__":
         #     plot_filename = os.path.join(plot_output_directory, f'6.30_potential_RCR_event_original_idx_{original_index}.png')
         #     pT(event_data, f'Potential RCR (Original Event Index: {original_index})', plot_filename)
         #     print(f"Plotting and saving event with original index {original_index} to {plot_filename}")
+
+    # from A1_TrainAndRunCNN import Train_CNN
+    # training_RCR, training_Backlobe, model_path, loss_accuracy_plot_path, timestamp, amp 
 
 
 

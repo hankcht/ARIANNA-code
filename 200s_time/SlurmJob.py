913c67df031dcf6f4ecf884f6f12543c0df8db9f
hankcht <hanktang930809@gmail.com>
2025-07-25 13:33:39 -0700

    m

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -80,10 +80,10 @@ if __name__ == "__main__":
 
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run.py'
     # RunMultipleJobs(cmd, jobName='train_and_run')
-    # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_checks.py'
-    # RunMultipleJobs(cmd, jobName='checks')
-    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/test_DANN.py'
-    RunMultipleJobs(cmd, jobName='test')
+    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_checks.py'
+    RunMultipleJobs(cmd, jobName='checks')
+    # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/test_DANN.py'
+    # RunMultipleJobs(cmd, jobName='test')
 
 
 # Set parameters

b9f80693b19543c96cb80f693ba3a9ff8944d44b
hankcht <hanktang930809@gmail.com>
2025-08-11 14:40:58 -0700

    test alternative setting batch job

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -9,14 +9,14 @@ def RunMultipleJobs(commandtorun, jobName='Batchjob', runDirectory='/pub/tangch3
     header += f"#SBATCH --job-name={jobName}         ##Name of the job.\n"
     header += "#SBATCH -A sbarwick_lab                 ##Account to charge to\n"
     header += "#SBATCH --partition=standard            ##Partition/queue name\n" 
-    header += "#SBATCH --time=0-10:00:00               ##Max runtime D-HH:MM:SS, 3 days free maximum\n"
+    header += "#SBATCH --time=0-01:00:00               ##Max runtime D-HH:MM:SS, 3 days free maximum\n"
     header += "#SBATCH --nodes=1                       ##Nodes to be used\n"
     
-    header += "#SBATCH --ntasks=5                     ##Number of processes to be launched (CPUs)\n" 
-    header += "#SBATCH --mem-per-cpu=6G                ##Requesting 6GB memory per CPU\n"
+    header += "#SBATCH --ntasks=5                      ##Number of processes to be launched (CPUs)\n" 
+    # header += "#SBATCH --mem-per-cpu=6G                ##Requesting 6GB memory per CPU\n"
     # If keep --mem, it will override --mem-per-cpu * --ntasks if it's a lower value.
     # It's better to explicitly calculate and rely on mem-per-cpu * ntasks for clarity.
-    # header += "#SBATCH --mem=18G\n" 
+    header += "#SBATCH --mem=18G\n" 
     
     header += "#SBATCH --output={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.out'))
     header += "#SBATCH --error={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.err'))

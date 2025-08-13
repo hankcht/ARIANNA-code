bd7dd196edd064f9acd6b03c522a967ce3ba1aad
hankcht <hanktang930809@gmail.com>
2025-08-13 15:24:46 -0700

    m

--- 200s_time/A0_Utilities.py
+++ 200s_time/A0_Utilities.py
@@ -528,8 +528,9 @@ if __name__ == "__main__":
 
     sim_rcr_730 = np.load('/dfs8/sbarwick_lab/ariannaproject/rricesmi/simulatedRCRs/7.30.25/200s/all_traces_200s_RCR_part0_50000events.npy', allow_pickle=True)
     print(f'number of traces is {len(sim_rcr_730)}')
-    sim_rcr_730 = np.array(sim_rcr_730.tolist()) 
+    sim_rcr_730 = np.stack([np.stack(row) for row in sim_rcr_730])
     print(sim_rcr_730.shape)
+    print(sim_rcr_730[0].shape)
     print(sim_rcr_730[0:2])
 
     # for i in range(5):

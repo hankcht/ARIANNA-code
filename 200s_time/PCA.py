843d284e835a1a351efe3a547e4dc376380e0448
hankcht <hanktang930809@gmail.com>
2025-08-13 13:58:11 -0700

    m

--- 200s_time/PCA.py
+++ 200s_time/PCA.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
 
     from refactor_checks import load_all_coincidence_traces, load_2016_backlobe_templates
     pkl_path = "/pub/tangch3/ARIANNA/DeepLearning/refactor/other/test_bandpass_on_coinc/filtered_coinc.pkl"
-    all_coincidence_events, _ = load_all_coincidence_traces(pkl_path, "Filtered_Traces")
+    coinc_dict, all_coincidence_events, _ = load_all_coincidence_traces(pkl_path, "Filtered_Traces")
 
     template_dir = "/dfs8/sbarwick_lab/ariannaproject/rricesmi/numpy_arrays/templates/confirmed2016Templates"
     template_paths = sorted(glob(os.path.join(template_dir, "Event2016_Stn*.npy")))

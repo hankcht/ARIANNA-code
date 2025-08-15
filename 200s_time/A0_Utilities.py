7163c8973485ac82b549fd256a9f21e3b853a30f
hankcht <hanktang930809@gmail.com>
2025-08-15 11:46:19 -0700

    plot 100s sim rcr and sim bl

--- 200s_time/A0_Utilities.py
+++ 200s_time/A0_Utilities.py
@@ -534,6 +534,10 @@ if __name__ == "__main__":
     sim_rcr_730 = np.load(f'/dfs8/sbarwick_lab/ariannaproject/rricesmi/simulatedRCRs/8.14.25/200s/all_traces_200s_RCR_part0_4473events.npy', allow_pickle=True) 
     sim_bl_730 = np.load(f'/dfs8/sbarwick_lab/ariannaproject/rricesmi/simulatedBacklobe/8.14.25/200s/all_traces_200s_part0_11239events.npy', allow_pickle=True)
 
+    sim_rcr_730_100s = np.load(f'/dfs8/sbarwick_lab/ariannaproject/rricesmi/simulatedRCRs/8.14.25/100s/all_traces_100s_RCR_part0_4200events.npy', allow_pickle=True) 
+    sim_bl_730_100s = np.load(f'/dfs8/sbarwick_lab/ariannaproject/rricesmi/simulatedBacklobe/8.14.25/100s/all_traces_100s_part0_9511events.npy', allow_pickle=True)
+
+
     config = load_config()
     from refactor_train_and_run import load_and_prep_data_for_training
     data = load_and_prep_data_for_training(config)
@@ -541,10 +545,10 @@ if __name__ == "__main__":
 
     # Usage
     indices = [2917, 2926, 4075, 4343] 
-    for i in indices:
-        # pT(sim_rcr_730[i], 'test plot 8/14 sim RCR', f"/pub/tangch3/ARIANNA/DeepLearning/refactor/other/test_plot_814_sim_rcr_{i}.png")
-        # pT(sim_bl_730[i], 'test plot 8/14 sim BL', f"/pub/tangch3/ARIANNA/DeepLearning/refactor/other/test_plot_814_sim_bl_{i}.png")
-        pT(sim_rcr_all[i], 'test plot 5/28 sim rcr', f"/pub/tangch3/ARIANNA/DeepLearning/refactor/other/test_plot_528_sim_rcr_{i}_noiseTrue.png")
+    for i in range(10,15):
+        pT(sim_rcr_730_100s[i], 'test plot 8/14 sim RCR', f"/pub/tangch3/ARIANNA/DeepLearning/refactor/other/test_plot_814_sim_rcr_{i}_100s.png")
+        pT(sim_bl_730_100s[i], 'test plot 8/14 sim BL', f"/pub/tangch3/ARIANNA/DeepLearning/refactor/other/test_plot_814_sim_bl_{i}_100s.png")
+        # pT(sim_rcr_all[i], 'test plot 5/28 sim rcr', f"/pub/tangch3/ARIANNA/DeepLearning/refactor/other/test_plot_528_sim_rcr_{i}_noiseTrue.png")
 
 
 

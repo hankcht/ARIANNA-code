521439dd1a5c1d044e65a2a1ab8fe6795fdd16dc
hankcht <hanktang930809@gmail.com>
2025-08-22 14:08:34 -0700

    train with filtered data

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -78,16 +78,16 @@ if __name__ == "__main__":
     # multi_run = True
     # main(multi_run=multi_run)
 
-    # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run.py'
-    # RunMultipleJobs(cmd, jobName='train_and_run')
+    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run.py' 
+    RunMultipleJobs(cmd, jobName='train_and_run') # check if the above curve is filtered!
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_checks.py'
     # RunMultipleJobs(cmd, jobName='checks')
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/PCA.py'
     # RunMultipleJobs(cmd, jobName='test')
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/A0_Utilities.py'
     # RunMultipleJobs(cmd, jobName='test')
-    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/apply_bandpass.py'
-    RunMultipleJobs(cmd, jobName='bandpass')
+    # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/apply_bandpass.py'
+    # RunMultipleJobs(cmd, jobName='bandpass')
 
 # Set parameters
 single_file = False # If True, we run each nur file individually (See DO4B1)

d1be5cd77fe8e9f3f06dc6b174bddf906fe05f4d
hankcht <hanktang930809@gmail.com>
2025-08-25 16:31:08 -0700

    save and plot filtered 1000 evt

--- 200s_time/apply_bandpass.py
+++ 200s_time/apply_bandpass.py
@@ -80,19 +80,19 @@ if __name__ == '__main__':
 
         filtered_traces_2016 = np.array(filtered_traces_2016)
         print(f"2016 Filtering complete. {s_id} Shape: {filtered_traces_2016.shape}")
-        # np.save(f'{output_dir}Stn{s_id}_Traces2016_above_filtered.npy', filtered_traces_2016)
+        np.save(f'{output_dir}Stn{s_id}_Traces2016_above_filtered.npy', filtered_traces_2016)
 
         filtered_traces_rcr = np.array(filtered_traces_rcr)
         print(f"RCR Filtering complete. {s_id} Shape: {filtered_traces_rcr.shape}")
-        # np.save(f'{output_dir}Stn{s_id}_TracesRCR_above_filtered.npy', filtered_traces_rcr)
+        np.save(f'{output_dir}Stn{s_id}_TracesRCR_above_filtered.npy', filtered_traces_rcr)
 
 
-        # testfilteredtraces2016 = np.load(f'{output_dir}Stn{s_id}_Traces2016_above_filtered.npy')
-        # print(f'LOADING NEW SAVED FROM {output_dir}Stn{s_id}_Traces2016_above_filtered.npy')
-        # from A0_Utilities import pT
-        # indices = [202, 510, 648, 763, 879]
-        # for index in indices:
-        #     pT(testfilteredtraces2016[index], f'plot filtered 2016 above cuvre stn {s_id}', f'/pub/tangch3/ARIANNA/DeepLearning/refactor/other/5000plot_filtered_data_{s_id}_{index}.png')
+        testfilteredtraces2016 = np.load(f'{output_dir}Stn{s_id}_Traces2016_above_filtered.npy')
+        print(f'LOADING NEW SAVED FROM {output_dir}Stn{s_id}_Traces2016_above_filtered.npy')
+        from A0_Utilities import pT
+        indices = [202, 510, 648, 763, 879]
+        for index in indices:
+            pT(testfilteredtraces2016[index], f'plot filtered 2016 above cuvre stn {s_id}', f'/pub/tangch3/ARIANNA/DeepLearning/refactor/other/1000plot_filtered_data_{s_id}_{index}.png')
 
 
 

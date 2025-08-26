57ee66f0889c5a1b0f2793320e07597a2c11b779
hankcht <hanktang930809@gmail.com>
2025-08-26 11:23:08 -0700

    max freq must be less than one half of sampling rate

--- 200s_time/apply_bandpass.py
+++ 200s_time/apply_bandpass.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     output_dir = '/pub/tangch3/ARIANNA/DeepLearning/refactor/confirmed_2016_templates/'
 
     sampling_rate_hz = 2 * units.GHz
-    passband = [0.05 * units.GHz, 1.0 * units.GHz]  # 50 MHz – 500 MHz
+    passband = [0.05 * units.GHz, 0.99 * units.GHz]  # 50 MHz – 500 MHz
     order = 2
 
     amp='200s'

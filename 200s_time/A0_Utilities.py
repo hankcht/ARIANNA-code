e06b5060b5f282c45e6e95e36dc4d0f72e97edd4
hankcht <hanktang930809@gmail.com>
2025-08-27 10:25:36 -0700

    check confirmed BL

--- 200s_time/A0_Utilities.py
+++ 200s_time/A0_Utilities.py
@@ -559,17 +559,24 @@ if __name__ == "__main__":
     # for i in range(4):
     #     pT(Above_curve_data_Traces2016[i], f'check filter index {i}', os.path.join(save_path, f"827_{num}evt_{i}.png"))
 
-    trace_type = 'Traces'
-    pkl_path = '/pub/tangch3/ARIANNA/DeepLearning/refactor/coincidence_events/filtered_coinc.pkl'
+    # trace_type = 'Traces'
+    # pkl_path = '/pub/tangch3/ARIANNA/DeepLearning/refactor/coincidence_events/filtered_coinc.pkl'
 
-    from refactor_checks import load_all_coincidence_traces
-    coinc_dict, coinc_traces, metadata = load_all_coincidence_traces(pkl_path, trace_key=trace_type) 
-    coinc_traces = np.array(coinc_traces)
-    print(coinc_traces.shape)
-    for i in np.arange(15,30):
-        pT(coinc_traces[i], f"test plot old coinc index {i}", f'/pub/tangch3/ARIANNA/DeepLearning/refactor/other/827_plot_coinc_{i}.png')
+    from refactor_checks import load_all_coincidence_traces, load_2016_backlobe_templates
+    # coinc_dict, coinc_traces, metadata = load_all_coincidence_traces(pkl_path, trace_key=trace_type) 
+    # coinc_traces = np.array(coinc_traces)
+    # print(coinc_traces.shape)
+    # for i in np.arange(15,30):
+    #     pT(coinc_traces[i], f"test plot old coinc index {i}", f'/pub/tangch3/ARIANNA/DeepLearning/refactor/other/827_plot_coinc_{i}.png')
 
+    config = load_config()
+    amp = config['amp']
+    template_dir = "/pub/tangch3/ARIANNA/DeepLearning/refactor/confirmed_2016_templates/"
+    template_paths = sorted(glob(os.path.join(template_dir, "filtered_Event2016_Stn*.npy"))) # using filtered confirmed BL
+    all_2016_backlobes, dict_2016 = load_2016_backlobe_templates(template_paths, amp_type=amp)
 
+    for i in np.arange(15,20):
+        pT(all_2016_backlobes[i], f"test plot 2016 BL index {i}", f'/pub/tangch3/ARIANNA/DeepLearning/refactor/other/827_plot_2016BL_{i}.png')
 
 
 

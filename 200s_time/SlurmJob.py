b8318cde62cb348175733851c2e389203ad77af0
hankcht <hanktang930809@gmail.com>
2025-08-28 11:45:49 -0700

    run checks

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -80,8 +80,8 @@ if __name__ == "__main__":
 
     cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run.py' 
     RunMultipleJobs(cmd, jobName='train_and_run') # check if the above curve is filtered!
-    # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_checks.py'
-    # RunMultipleJobs(cmd, jobName='checks')
+    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_checks.py'
+    RunMultipleJobs(cmd, jobName='checks')
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/PCA.py'
     # RunMultipleJobs(cmd, jobName='test')
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/A0_Utilities.py'

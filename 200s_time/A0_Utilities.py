0baf52cc0a26dd579f3f8b5e7bc8c3543f7d4449
hankcht <hanktang930809@gmail.com>
2025-09-02 10:52:32 -0700

    m

--- 200s_time/A0_Utilities.py
+++ 200s_time/A0_Utilities.py
@@ -566,18 +566,16 @@ if __name__ == "__main__":
     templates_200_2016 = loadMultipleTemplates(series=200, date='2016')
     templates_100_2016 = loadMultipleTemplates(series=100, date='2016')
 
-    print(new_metadata[1297]['ChiRCR'])
-    print(new_metadata[1298]['ChiRCR'])
-    print(new_metadata[1297]['Chi2016'])
-    print(new_metadata[1299]['Chi2016'])
+    print(f'UNFILTERED {(old_metadata[1297]['ChiRCR'],old_metadata[1298]['ChiRCR'])} {(old_metadata[1297]['Chi2016'], old_metadata[1298]['Chi2016'])}')
+    print(f'FILTERED {(new_metadata[1297]['ChiRCR'],new_metadata[1298]['ChiRCR'])} {(new_metadata[1297]['Chi2016'], new_metadata[1298]['Chi2016'])}')
 
     chiRCR_200 = getMaxAllChi(coinc_traces[1298], 2*units.GHz, templates_200_RCR, 2*units.GHz)
     chiRCR_100 = getMaxAllChi(coinc_traces[1297], 2*units.GHz, templates_100_RCR, 2*units.GHz)
-    print((chiRCR_100,chiRCR_200))
 
     chi2016_200 = getMaxAllChi(coinc_traces[1298], 2*units.GHz, templates_200_2016, 2*units.GHz)
     chi2016_100 = getMaxAllChi(coinc_traces[1297], 2*units.GHz, templates_100_2016, 2*units.GHz)
-    print((chi2016_100,chi2016_200))
+
+    print(f'CALCULATED FILTERED {(chiRCR_100, chiRCR_200)} {(chi2016_100, chi2016_200)}')
     # print(chi2016_200)
 
     # for i in indices:

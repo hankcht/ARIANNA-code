19df0964aec31f2b7e51b7486e9ea8ecc26963bc
hankcht <hanktang930809@gmail.com>
2025-10-07 15:32:40 -0700

    eval checks

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -80,12 +80,12 @@ if __name__ == "__main__":
 
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_converter.py'
     # RunMultipleJobs(cmd, jobName='converter')
-    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run.py' 
-    RunMultipleJobs(cmd, jobName='train_and_run') # check if the above curve is filtered!
+    # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run.py' 
+    # RunMultipleJobs(cmd, jobName='train_and_run') # check if the above curve is filtered!
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run_all_amp.py' 
     # RunMultipleJobs(cmd, jobName='train_and_run') # check if the above curve is filtered!
-    # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_checks.py'
-    # RunMultipleJobs(cmd, jobName='checks')
+    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_checks.py'
+    RunMultipleJobs(cmd, jobName='checks')
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/test_1d_CNN.py'
     # RunMultipleJobs(cmd, jobName='train_and_run')
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/PCA.py'

59666db2dbd939004b66f192fd118500355aa2dc
hankcht <hanktang930809@gmail.com>
2025-10-07 13:30:37 -0700

    fix typo

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     # multi_run = True
     # main(multi_run=multi_run)
 
-    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_cconverter.py'
+    cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_converter.py'
     RunMultipleJobs(cmd, jobName='converter')
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_train_and_run.py' 
     # RunMultipleJobs(cmd, jobName='train_and_run') # check if the above curve is filtered!

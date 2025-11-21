69a4e155b5a9b8638f45ee5a0a53ed663ce4153a
rricesmith <rricesmith@gmail.com>
2025-11-21 14:36:46 -0800

    Skip sim rcr

--- model_runners/R04_VAE_train_and_run.py
+++ model_runners/R04_VAE_train_and_run.py
@@ -2113,6 +2113,7 @@ def main():
     # 2. Normalize Evaluation Data
     # We MUST keep these stats to reverse the plots later!
     sim_rcr_all, rcr_stats = normalizer.normalize(sim_rcr_all)
+    sim_rcr_all = None  # Trying out not using sim rcr at all in plots
     data_backlobe_traces_rcr_all, bl_stats = normalizer.normalize(data_backlobe_traces_rcr_all)
     
     print(f"Data Normalized. Training shape: {training_backlobe.shape}")

ca37eae558c37487940589300ea764b1a999b772
hankcht <hanktang930809@gmail.com>
2025-12-18 21:04:20 -0800

    correctly run continued, removed fr setting for oq_conf, kbi for both instead of kif for datalane

--- 200s_time/HGQ_1D_CNN.py
+++ 200s_time/HGQ_1D_CNN.py
@@ -83,11 +83,11 @@ def build_hgq_model(input_shape, ramp_epochs, beta0=1e-5, beta_final=1e-4):
 
 
     # Define Config Scopes 
-    scope0 = QuantizerConfigScope(place='all', b0=3, i0=0, default_q_type='kbi', overflow_mode='SAT_SYM') #  k0=1,
-    scope1 = QuantizerConfigScope(place='datalane', f0=3, i0=3, default_q_type='kif', overflow_mode='SAT_SYM') # 
+    scope0 = QuantizerConfigScope(place='all', default_q_type='kbi', overflow_mode='SAT_SYM') # b0=3, i0=0, k0=1,
+    scope1 = QuantizerConfigScope(place='datalane', default_q_type='kbi', overflow_mode='WRAP') # , f0=3, i0=3
     with scope0, scope1: 
         iq_conf = QuantizerConfig(place='datalane') # input quantizer
-        oq_conf = QuantizerConfig(place='datalane', fr=MonoL1(1e-3)) # output quantizer   
+        oq_conf = QuantizerConfig(place='datalane') # output quantizer   
         model = keras.Sequential([
                     keras.layers.Input(shape=input_shape),
                     QConv1D(20, kernel_size=10, beta0=beta0, iq_conf=iq_conf, activation='relu', name='conv1d_0'),

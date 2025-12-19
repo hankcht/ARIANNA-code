42beddf763efd5cfd09f16593c509d7454d5ef41
hankcht <hanktang930809@gmail.com>
2025-12-19 11:42:46 -0800

    correctly run Go back to the original model and use SAT_SYM for datalane

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -80,17 +80,17 @@ def main(multi_run):
 
 
 if __name__ == "__main__":
-    multi_run = False
-    main(multi_run=multi_run)
+    # multi_run = False
+    # main(multi_run=multi_run)
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_converter.py'
     # RunMultipleJobs(cmd, jobName='converter')
 
     # cmd = f"python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/HGQ_test_smalljet.py"
     # RunMultipleJobs(cmd, jobName='HGQ2_small_jet', python_env='hgq2')
-    # epochs = [1711, 2011, 2211] # 30, 50, 80, 100, 300, 500, 850, 1000, 1200, 1500, 1800, 2000
-    # for epoch in epochs: 
-    #     cmd = f"python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/HGQ_1D_CNN.py --epochs {epoch}"
-    #     RunMultipleJobs(cmd, jobName=f'HGQ2_{epoch}', python_env='hgq2')
+    epochs = [1711, 2011, 2211] # 30, 50, 80, 100, 300, 500, 850, 1000, 1200, 1500, 1800, 2000
+    for epoch in epochs: 
+        cmd = f"python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/HGQ_1D_CNN.py --epochs {epoch}"
+        RunMultipleJobs(cmd, jobName=f'HGQ2_{epoch}', python_env='hgq2')
 
 
     # # Define available model types

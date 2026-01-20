103e70e16501d5937af3da5bb02a8f90371071a7
hankcht <hanktang930809@gmail.com>
2026-01-20 13:43:40 -0800

    test get real thermal noise

--- 200s_time/SlurmJob.py
+++ 200s_time/SlurmJob.py
@@ -88,11 +88,16 @@ if __name__ == "__main__":
     # for epoch in epochs: 
     #     cmd = f"python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/HGQ_1D_CNN.py --epochs {epoch}"
     #     RunMultipleJobs(cmd, jobName=f'HGQ2_{epoch}', python_env='hgq2')
-    epoch = 2500
-    for seed in range(66,70):
-        cmd = f"python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/HGQ_1D_CNN.py --epochs {epoch} --seed {seed}"
-        RunMultipleJobs(cmd, jobName=f'HGQ2_{epoch}_{seed}', python_env='hgq2')
+    # epoch = 2500
+    # for seed in range(66,70):
+    #     cmd = f"python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/HGQ_1D_CNN.py --epochs {epoch} --seed {seed}"
+    #     RunMultipleJobs(cmd, jobName=f'HGQ2_{epoch}_{seed}', python_env='hgq2')
 
+    station_id = 14
+    forced_trigger_files = f'/pub/rricesmi/Arianna/ReflectiveAnalysis/StationDataAnalysis/processedNur/station_{station_id}/forced_triggers_*.nur'
+    cmd = f"python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/nu_sim/plotProcessedNur.py --station {station_id} --num_traces {20} \
+    --list {forced_trigger_files}"
+    RunMultipleJobs(cmd, jobName=f'forced_trigger_{station_id}')    
 
     # cmd = 'python /pub/tangch3/ARIANNA/DeepLearning/code/200s_time/refactor_converter.py'
     # RunMultipleJobs(cmd, jobName='converter')

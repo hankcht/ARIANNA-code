ec973877e1f0df7e470521ac0581e99540e25fe7
hankcht <hanktang930809@gmail.com>
2026-01-21 10:52:19 -0800

    remove all chi calculations

--- 200s_time/nu_sim/plotProcessedNur.py
+++ 200s_time/nu_sim/plotProcessedNur.py
@@ -64,22 +64,11 @@ for file in filesToRead:
 
         channelBandPassFilter.run(evt, station, det, passband=[1*units.Hz, np.inf*units.MHz])	#Remove DC bias
 
-        nu_xCorr = 0
-        cr_xCorr = 0
-
         # for parChans in parallelChannels:
         #     print(f'parallel channels are: {parChans}')
-        nu_avgCorr = []
-        cr_avgCorr = []
+
 
         for channel in station.iter_channels(): # use_channels=parChans
-            if channel.has_parameter(chp.nu_xcorrelations):
-                print(f'Calculating chi values')
-                nu_avgCorr.append(np.abs(channel.get_parameter(chp.nu_xcorrelations)))
-                cr_avgCorr.append(np.abs(channel.get_parameter(chp.cr_xcorrelations)))
-            else:
-                nu_avgCorr.append(0)
-                cr_avgCorr.append(0)
             
             # Save traces for forced triggers only (no SNR/Chi)
             if tracesPlotted < num_traces:
@@ -100,49 +89,6 @@ for file in filesToRead:
                 plt.clf()
 
                 tracesPlotted += 1
-                
 
-            nu_avgCorr = np.mean(np.abs(nu_avgCorr))
-            cr_avgCorr = np.mean(np.abs(cr_avgCorr))
 
-            if nu_avgCorr > nu_xCorr:
-                nu_xCorr = nu_avgCorr
-            if cr_avgCorr > cr_xCorr:
-                cr_xCorr = cr_avgCorr
 
-        # Store correlation values and forced triggers
-        nu_maxCorr.append(nu_xCorr)
-        nu_forcedMask.append(station.has_triggered())
-        cr_maxCorr.append(cr_xCorr)
-        cr_forcedMask.append(station.has_triggered())
-
-nu_maxCorr = np.array(nu_maxCorr)
-cr_maxCorr = np.array(cr_maxCorr)
-nu_forcedMask = np.array(nu_forcedMask)
-cr_forcedMask = np.array(cr_forcedMask)
-
-# Correlation scatter plot (no Chi)
-plt.scatter(nu_maxCorr[np.logical_not(nu_forcedMask)], cr_maxCorr[np.logical_not(nu_forcedMask)], facecolors='none', edgecolor='black', label='Non-forced')
-plt.scatter(nu_maxCorr[nu_forcedMask], cr_maxCorr[cr_forcedMask], label=f'Forced Events {len(nu_maxCorr[nu_forcedMask])}')
-plt.xlim((0, 1))
-plt.ylim((0, 1))
-plt.xlabel('Nu Corr')
-plt.ylabel('CR Corr')
-plt.legend()
-plt.title(f'Station {station_id} Nu vs CR Correlations')
-plt.savefig(f'/pub/tangch3/ARIANNA/DeepLearning/true_therm_noise/StationDataAnalysis/plots/station{station_id}_NuCR_corr.png')
-plt.clf()
-
-# Save processed data
-data_dump = {
-    f'station_{station_id}': {
-        'station_id': station_id,
-        'nu_xCorr': nu_maxCorr,
-        'cr_xCorr': cr_maxCorr,
-        'nu_trigMask': nu_forcedMask,
-        'cr_trigMask': cr_forcedMask
-    }
-}
-
-with open(f'/pub/tangch3/ARIANNA/DeepLearning/true_therm_noise/StationDataAnalysis/processedPkl/data_station_{station_id}.pkl', 'wb') as output:
-    pickle.dump(data_dump, output)
